dcef95cc0f4847f8beda6239864d85c9372fe12f
r3b3l-dev <wayne.manning@tuta.io>
2022-05-23 07:58:42 +0000

    Add function layout structure using comments

--- run.py
+++ run.py
@@ -1,3 +1,43 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# imports
+
+# console needs to be defined for rich
+
+# pyfiglet message variables 
+
+# clear  the terminal
+
+# fucntion to simulate firing sequence
+
+# main grid class
+
+# helper function for random coordinates
+
+# helper function row and col within bounds and hit or miss
+
+# helper function places ships on grid
+
+#helper function is ship already there
+
+# player guess row validation 
+
+# player guess col validation
+
+# player guess helper function
+
+# player input validation and return guess
+
+# checks if input is valid and returns computer guess
+
+# sets the grid
+
+# displays the grid
+
+# end of game function
+
+# next round 
+
+# welcome message
+
+# run the game
+
+# name == main
